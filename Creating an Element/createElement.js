6f881aead5287bb7e701e65b60e4447920d8974e
Dhaval <joshidhaval2002@gmail.com>
2023-09-12 23:35:55 +0530

    added button n=inside the div

--- Creating an Element/createElement.js
+++ Creating an Element/createElement.js
@@ -1,10 +1,14 @@
-const appendDiv=document.createElement('div')
+const div=document.createElement('div')
 
 const body=document.body   //used to access the body element of html
-body.appendChild(appendDiv)
+body.appendChild(div)
 
 const paragraph=document.createElement('p')   //created Element P inside JS
 paragraph.textContent='THis is a paragraph wriiten inside JS'   //added cpntent inside the p tag
-appendDiv.appendChild(paragraph)            //added p  inside the div
+div.appendChild(paragraph)            //added p  inside the div
 
 //Now creating button through JS which will display in webpage
+
+const btn=document.createElement('button')
+btn.textContent='Click Here';
+div.appendChild(btn)
