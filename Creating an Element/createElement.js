8c9805a3a0a887ff44fd859801d11792a3eb8713
Dhaval <joshidhaval2002@gmail.com>
2023-09-12 23:33:12 +0530

    Creating button using createElement

--- Creating an Element/createElement.js
+++ Creating an Element/createElement.js
@@ -5,4 +5,6 @@ body.appendChild(appendDiv)
 
 const paragraph=document.createElement('p')   //created Element P inside JS
 paragraph.textContent='THis is a paragraph wriiten inside JS'   //added cpntent inside the p tag
-appendDiv.appendChild(paragraph)            //added p  insidee the div
+appendDiv.appendChild(paragraph)            //added p  inside the div
+
+//Now creating button through JS which will display in webpage
