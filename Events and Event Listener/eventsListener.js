8c183e1be4e84526012610de7c05c798098824ad
Dhaval <joshidhaval2002@gmail.com>
2023-09-14 20:40:52 +0530

    eventListener in JS

---
+++ Events and Event Listener/eventsListener.js
@@ -0,0 +1,21 @@
+console.log("HEllo")
+const h2=document.getElementsByClassName('heading2');
+// const h2=document.querySelectorAll('h2').innerHTML;
+console.log(h2)
+console.log(h2[1])
+const indexONe=[h2[0],h2[1]]
+
+
+//Note getElements with ID will the full html elements and a single element because ID are unique
+
+const para=document.getElementById('para').innerHTML  //to get the content inside the element tag
+console.log(para)
+
+const paragraph=document.getElementById('para').textContent  //to get the content inside the element tag
+console.log(paragraph)
+
+indexONe.addEventListener('click',toAlert)
+
+function toAlert(){
+    alert('You have entered heading 2')
+}
