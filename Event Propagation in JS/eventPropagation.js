739aa0ba7ba880deb5b3a46eb15e9d11a0133412
Dhaval <joshidhaval2002@gmail.com>
2023-09-16 19:45:21 +0530

    Bubbling in js example

---
+++ Event Propagation in JS/eventPropagation.js
@@ -0,0 +1,13 @@
+document.getElementById('h1').addEventListener('click',function(){
+    console.log('INNER HEADING CLICKED')
+})
+// The above will print the inner heading clicked when clicked on the inner heading
+
+//Now the bubbling concept
+
+document.getElementById('innerdiv').addEventListener('click',()=>{
+    console.log('Inner Div Clicked')
+})
+
+//Now here what happens when i click Inner heading also the it console the inner heading clicked as well as 'Inner Div Clicked' though i have not clicked inner div
+//This is called bubbling the event will also run the parent event also -->in this case the parent is inner div of inner heading so inner div event is executed
